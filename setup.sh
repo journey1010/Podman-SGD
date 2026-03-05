e7b33155b1d45ba3a7f0e4e42c27ca6ccebfb1df
journii <ginopaflo001608@gmail.com>
2026-03-04 20:41:21 -0500

    update setup

--- setup.sh
+++ setup.sh
@@ -4,3 +4,4 @@ mv payara-5.2022.5.zip payara/payara-5.2022.5.zip
 
 echo "Descargando WARS, JARs y Scripts"
 wget https://github.com/journey1010/Podman-SGD/releases/download/Files/PostgreSQL.zip
+mv PostgreSQL.zip postgres/PostgreSQL.zip
