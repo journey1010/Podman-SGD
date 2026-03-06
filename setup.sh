6a041d82278935cc5ee3c90a273840ba01c71f20
journii gorle <ginopaflo001608@gmail.com>
2026-03-06 10:43:29 -0500

    fix: payara image

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 echo "Descargando Payara 5.2022.5"
-wget https://github.com/journey1010/Podman-SGD/releases/download/Files/payara-5.2022.5.zip
-mv payara-5.2022.5.zip payara/payara-5.2022.5.zip
+wget https://github.com//journey1010/sgd-container/releases/download/Files/payara5.zip
+mv payara5.zip payara/payara5.zip
 
 echo "Descargando WARS, JARs y Scripts"
-wget https://github.com/journey1010/Podman-SGD/releases/download/Files/PostgreSQL.zip
+wget https://github.com//journey1010/sgd-container/releases/download/Files/PostgreSQL.zip
 mv PostgreSQL.zip postgres/PostgreSQL.zip
